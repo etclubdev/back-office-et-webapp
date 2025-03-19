beb245ee27342da5bb21be4b4140e322ad1e5253
avytran <tr.anhvy.work@gmail.com>
2025-03-20 00:52:46 +0700

    feat(P2BI-244): add Header component

---
+++ front-end/src/constants.js
@@ -0,0 +1,13 @@
+const breadcrumbNameMap = {
+    "account": "Quản lý tài khoản",
+    "hr": "Quản lý nhân sự",
+    "partner": "Quản lý đối tác",
+    "homepage": "Quản lý trang chủ",
+    "achievement": "Thành tựu",
+    "partner": "Đối tác",
+    "faqs": "Lựa chọn FAQs",
+    "post": "Quản lý bài đăng",
+    "collaborator": "Quản lý Tìm kiếm CTV",
+};
+
+export { breadcrumbNameMap }
