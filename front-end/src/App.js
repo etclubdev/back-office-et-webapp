7aa94f5b41f75b0cd6aee80fa367db044456608e
avytran <tr.anhvy.work@gmail.com>
2025-03-27 14:36:21 +0700

    refactor(P2BI-245): centralize ConfirmedDialog logic using a utility

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import React, { useState } from 'react';
-import { ConfirmedDialog } from './components/ConfirmedDialog';
+import React from 'react';
+// import { UsingDialogPage } from './pages/UsingDialogPage/UsingDialogPage';
 
 function App() {
-  // const [isOpen, setIsOpen] = useState(true);
-  // const [confirmed, setConfirmed] = useState(false);
-  
   return (
     <div className="App">
       <Router>
         <div className="root-container">
           <Routes>
-            {/* <Route path="/" element={
-              <ConfirmedDialog 
-                title="Xác nhận xóa thành tựu" 
-                desc="Bạn chắc chắn muốn xóa thành tựu này? Sau khi xóa, bạn sẽ không thể khôi phục lại" 
-                type="delete"
-                isOpen={isOpen}
-                onClose={() => setIsOpen(false)}
-                onConfirm={() => setConfirmed(true)}
-              />} /> */}
+
           </Routes>
         </div>
       </Router>
--- front-end/src/components/ConfirmedDialog/ConfirmedDialog.jsx
+++ front-end/src/components/ConfirmedDialog/ConfirmedDialog.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
 import './ConfirmedDialog.css';
 
-export const ConfirmedDialog = ({ title, desc, type, isOpen = true, onClose, onConfirm }) => {
-    const handleConfirm = () => {
-        onConfirm();
-        onClose();
-    }
-
-    return isOpen && (
+export const ConfirmedDialog = ({ title, desc, alertType, onClose, onConfirm }) => {
+    return (
         <div className="modal-overlay">
             <div className="modal-container">
                 <div className="modal-close">
                     <button className="close-button" onClick={onClose}>✕</button>
                 </div>
                 <div className="modal-icon">
-                    <div className={`circle ${type}`}>
+                    <div className={`circle ${alertType}`}>
                         <span className="mark">!</span>
                     </div>
                 </div>
@@ -24,7 +19,7 @@ export const ConfirmedDialog = ({ title, desc, type, isOpen = true, onClose, onC
                 </div>
                 <div className="modal-actions">
                     <button className="cancel-button" onClick={onClose}>Hủy bỏ</button>
-                    <button className={`confirm-button ${type}`} onClick={handleConfirm}>Xác nhận</button>
+                    <button className={`confirm-button ${alertType}`} onClick={onConfirm}>Xác nhận</button>
                 </div>
             </div >
         </div >
