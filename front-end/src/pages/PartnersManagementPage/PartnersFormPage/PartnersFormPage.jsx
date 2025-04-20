11e92b5b095740597b7807f624804b57bf67f0a7
avytran <tr.anhvy.work@gmail.com>
2025-04-20 07:27:46 +0700

    feat(P2BI-562): add partner management screen

---
+++ front-end/src/pages/PartnersManagementPage/PartnersFormPage/PartnersFormPage.jsx
@@ -0,0 +1,190 @@
+import React, { useEffect, useState } from 'react';
+import "./PartnersFormPage.css";
+import { useParams, useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { FormControl, Button } from "@mui/material";
+
+import { SwitchController } from '../../../components/SwitchController'
+import { SelectController } from '../../../components/SelectController';
+import { ImageUploadController } from '../../../components/ImageUploadController';
+import { TextFieldController } from '../../../components/TextFieldController';
+import { Header } from "../../../components/Header";
+import { getPartnerById, updatePartner, createPartner } from '../../../api/partner.service';
+import { partnerSchema } from '../../../schemas/partnerSchema';
+import { ConfirmedDialog } from '../../../components/ConfirmedDialog';
+import { handleImageUpload } from '../../../utils/handleUploadUtil';
+
+export const PartnersFormPage = ({ action }) => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [defaultItems, setDefaultItems] = useState({
+        "partner_name": "",
+        "partner_category_name": "",
+        "avatar_url": "",
+        "short_description": "",
+        "email": "",
+        "phone_number": "",
+        "note": "",
+        "visible": true,
+    });
+
+    const [preview, setPreview] = useState(null);
+    const [isOpenDialog, setIsOpenDialog] = useState(false);
+
+    const {
+        control,
+        handleSubmit,
+        setValue,
+        reset,
+        formState: { errors, isSubmitting },
+    } = useForm({
+        resolver: yupResolver(partnerSchema),
+        defaultValues: defaultItems,
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (id) {
+                const { data } = await getPartnerById(id);
+                setDefaultItems(data);
+            }
+        };
+        fetchData();
+    }, [id]);
+
+    useEffect(() => {
+        if (defaultItems && Object.keys(defaultItems).length > 0) {
+            reset(defaultItems);
+        }
+    }, [defaultItems, reset]);
+
+    const handleCancel = () => {
+        navigate('/partners');
+    }
+
+    const handleClose = () => {
+        setIsOpenDialog(false);
+    }
+
+    const onSubmit = async (payload) => {
+        try {
+            let avatarUrl = await handleImageUpload(payload.avatar_url, "avatar_url", preview, setPreview, setValue);
+
+            const fullPayload = { ...payload, avatar_url: avatarUrl };
+            console.log(fullPayload);
+            
+            if (action === "create") {
+                const response = await createPartner(fullPayload);
+                setIsOpenDialog(true);
+            } else if (action === "edit") {
+                const response = await updatePartner(id, fullPayload);
+                setIsOpenDialog(true);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    return (
+        <div className="form-page">
+            {
+                isOpenDialog && (
+                    <ConfirmedDialog
+                        title={`${action === "create" ? "Thêm" : "Sửa"} thành công`}
+                        alertType="info"
+                        onClose={handleClose}
+                    />
+                )
+            }
+            <Header>{action === "create" ? "Thêm Đối tác mới" : "Chỉnh sửa Đối tác"}</Header>
+            <div className="form-container">
+                <div className="form">
+                    <form onSubmit={handleSubmit(onSubmit)} >
+                        <div className="form-top">
+                            <div className="form-left">
+                                <ImageUploadController
+                                    name="avatar_url"
+                                    control={control}
+                                    preview={preview}
+                                    setPreview={setPreview}
+                                    setValue={setValue}
+                                    errors={errors}
+                                />
+
+                            </div>
+
+                            <div className="form-right">
+                                <SwitchController
+                                    name="visible"
+                                    control={control}
+                                    label="Hiển thị trang chủ"
+                                />
+
+                                <TextFieldController
+                                    name="partner_name"
+                                    control={control}
+                                    label="Tên Đối tác"
+                                    errors={errors}
+                                />
+
+                                <SelectController
+                                    name="partner_category_name"
+                                    control={control}
+                                    label="Danh mục Đối tác"
+                                    menuItems={[
+                                        "Đối tác doanh nghiệp",
+                                        "Đối tác chuyên gia",
+                                        "Đối tác Truyền thông",
+                                        "Nghệ sĩ khách mời"
+                                    ]}
+                                    errors={errors}
+                                />
+
+                                <TextFieldController
+                                    name="short_description"
+                                    control={control}
+                                    label="Mô tả"
+                                    errors={errors}
+                                />
+
+                                <TextFieldController
+                                    name="email"
+                                    control={control}
+                                    label="Email"
+                                    errors={errors}
+                                />
+
+                                <TextFieldController
+                                    name="phone_number"
+                                    control={control}
+                                    label="Số điện thoại"
+                                    errors={errors}
+                                />
+
+                                <TextFieldController
+                                    name="note"
+                                    control={control}
+                                    label="Ghi chú"
+                                    errors={errors}
+                                />
+                            </div>
+                        </div>
+                        <div className="form-bottom">
+                            <div className="form-bottom">
+                                <div className="form-button">
+                                    <Button disabled={isSubmitting} variant="outlined" color="primary" onClick={handleCancel}>
+                                        Hủy bỏ
+                                    </Button>
+                                    <Button disabled={isSubmitting} type="submit" variant="contained" color="primary">
+                                        {isSubmitting ? "Đang lưu..." : "Gửi"}
+                                    </Button>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
