672d41f8fd8c4a2b5f5bfa78dc32f28efc4c76ff
avytran <tr.anhvy.work@gmail.com>
2025-04-20 07:47:24 +0700

    feat(P2BI-502): add faqs management screen

---
+++ front-end/src/pages/FAQsManagementPages/FAQsFormPage/FAQsFormPage.jsx
@@ -0,0 +1,135 @@
+import React, { useEffect, useState } from 'react';
+import "./FAQsFormPage.css";
+import { useParams, useNavigate } from "react-router-dom";
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { TextField, Select, MenuItem, FormControl, InputLabel, Switch, FormControlLabel, Button } from "@mui/material";
+import { createFAQSchema, updateFAQSchema } from "../../../schemas/faqsSchema";
+
+import { Header } from "../../../components/Header";
+
+import { createFAQs, updateFAQsById, getFAQsById } from '../../../api/faq.service';
+
+export const FAQsFormPage = ({ action }) => {
+    const navigate = useNavigate();
+    const { id } = useParams();
+    const [defaultFAQs, setDefaultFAQs] = useState({
+        question: "",
+        faq_category: "",
+        answer: "",
+        visible: false,
+    });
+    
+    const {
+        control,
+        handleSubmit,
+        reset,
+        formState: { errors },
+    } = useForm({
+        resolver: yupResolver(action === "create" ? createFAQSchema : updateFAQSchema),
+        defaultValues: id ? defaultFAQs : {
+            question: "",
+            faq_category: "",
+            answer: "",
+            visible: false,
+        },
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            if (id) {
+                const data = await getFAQsById(id);
+                setDefaultFAQs(data);
+            }
+        };
+        fetchData();
+    }, [id]);
+    
+    useEffect(() => {
+        if (defaultFAQs && Object.keys(defaultFAQs).length > 0) {
+            reset(defaultFAQs);
+        }
+    }, [defaultFAQs, reset]);
+
+    const handleCancel = () => {
+        navigate('/faqs');
+    }
+
+    const onSubmit = async (payload) => {
+        if (action === "create") {
+            const response = await createFAQs(payload);
+            navigate('/faqs');
+        } else if (action === "edit") {
+            const response = await updateFAQsById(id, payload);
+            navigate('/faqs');
+        }
+    };
+
+    return (
+        <div className="faqs-form-page">
+            <Header>{action === "create" ? "Thêm FAQs mới" : "Chỉnh sửa FAQs"}</Header>
+            <div className="faqs-form">
+                <form onSubmit={handleSubmit(onSubmit)} >
+                    <div className="faqs-form-top">
+                        <div className="faqs-form-left">
+                            <Controller
+                                name="question"
+                                control={control}
+                                render={({ field }) => (
+                                    <TextField {...field} label="Câu hỏi" fullWidth error={!!errors.question} helperText={errors.question?.message} required />
+                                )}
+                            />
+
+                            <FormControl fullWidth error={!!errors.faq_category}>
+                                <Controller
+                                    name="faq_category"
+                                    control={control}
+                                    render={({ field }) => (
+                                        <>
+                                            <InputLabel>Nhóm câu hỏi</InputLabel>
+                                            <Select {...field} label="Nhóm câu hỏi">
+                                                <MenuItem value="Về ET Club">Về ET Club</MenuItem>
+                                                <MenuItem value="Về hoạt động và sự kiện">Về hoạt động và sự kiện</MenuItem>
+                                                <MenuItem value="Về quy trình tham gia">Về quy trình tham gia</MenuItem>
+                                                <MenuItem value="Khác">Khác</MenuItem>
+                                            </Select>
+                                        </>
+                                    )}
+                                />
+                            </FormControl>
+                        </div>
+                        <div className="faqs-form-right">
+                            <Controller
+                                name="visible"
+                                control={control}
+                                render={({ field }) => (
+                                    <FormControlLabel control={<Switch {...field} checked={field.value} />} label="Hiển thị trang chủ" />
+                                )}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="faqs-form-bottom">
+                        <Controller
+                            name="answer"
+                            control={control}
+                            render={({ field }) => (
+                                <TextField {...field} label="Câu trả lời" multiline rows={8} fullWidth error={!!errors.answer} helperText={errors.answer?.message} required />
+                            )}
+                        />
+
+                        <div className="faqs-form-button">
+                            <Button variant="outlined" color="primary" onClick={handleCancel}>
+                                Hủy bỏ
+                            </Button>
+                            <Button type="submit" variant="contained" color="primary">
+                                Lưu
+                            </Button>
+                        </div>
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
